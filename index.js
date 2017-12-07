76001c88b44614b5f9b23d6e5f81dcc84f2af711
dss <787938906@qq.com>
2017-12-07 17:12:34 +0800

    Site updated: 2017-12-07 17:12:33

---
+++ index.js
@@ -0,0 +1,10 @@
+// window.onload = function(){
+// 	var bgImg = [
+// 		"../images/bg5.jpg",
+// 		"../images/bg6.JPG",
+// 		"../images/bg7.jpg"
+// 	];
+// 	var bgIndex = Math.round( Math.random() * 2 );
+// 	var bg = document.getElementsByClassName('home-bg');
+// 	bg[0].style.backgroundImage = 'url('+ bgImg[bgIndex] +')';
+// }
